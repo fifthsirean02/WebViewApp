1954bc573566801ae42078003d44f5ff60efb28c
nishant <nishjan1996@gmail.com>
2017-12-10 01:51:45 +0530

    Improved Kotlin Code

--- app/src/main/java/com/kotlin/webapp/webviewapp/KotlinActivity.kt
+++ app/src/main/java/com/kotlin/webapp/webviewapp/KotlinActivity.kt
@@ -1,11 +1,14 @@
 package com.kotlin.webapp.webviewapp
 
+import android.content.Context
+import android.net.ConnectivityManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.webkit.JavascriptInterface
 import android.webkit.WebChromeClient
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_kotlin.*
 
 class KotlinActivity : AppCompatActivity() {
@@ -15,17 +18,24 @@ class KotlinActivity : AppCompatActivity() {
         setContentView(R.layout.activity_kotlin)
 
 
-        WebView.setWebContentsDebuggingEnabled(true)                    // STEP - 02
-        myWebView.settings.javaScriptEnabled = true                     // STEP - 03
-        myWebView.addJavascriptInterface(JavaScriptInterface(), J_OBJ)  // STEP - 04
-        myWebView.webChromeClient = object : WebChromeClient() {}       // STEP - 05
-        myWebView.webViewClient = object : WebViewClient() {            // STEP - 06
+        WebView.setWebContentsDebuggingEnabled(true)                        // STEP - 02
+        myWebView.settings.javaScriptEnabled = true                         // STEP - 03
+        myWebView.addJavascriptInterface(JavaScriptInterface(), J_OBJ)      // STEP - 04
+        myWebView.webChromeClient = object : WebChromeClient() {}           // STEP - 05
+        myWebView.webViewClient = object : WebViewClient() {                // STEP - 06
             override fun onPageFinished(view: WebView?, url: String?) {     // STEP - 06
                 var javaScript: String = "javascript: try { jObj = eval($J_OBJ); } catch(err) { jObj = window; }"
                 myWebView.loadUrl(javaScript)
             }
         }
-        myWebView.loadUrl(INNER_URL)                                     // STEP - 07
+
+        if (isNetworkAvailable) {                                           // STEP - 07
+            myWebView.loadUrl(BASE_URL)
+            toast("Online Mode : Loading Live Page ...")
+        } else {
+            myWebView.loadUrl(INNER_URL)
+            toast("Offline Mode : Loading App Page ...")
+        }
 
         btn.setOnClickListener {
             sendToJS()
@@ -34,13 +44,13 @@ class KotlinActivity : AppCompatActivity() {
     } // End of fun onCreate()
 
 
-    override fun onDestroy() {                                          // STEP - 08
+    override fun onDestroy() {                                              // STEP - 08
         myWebView.removeJavascriptInterface(J_OBJ)
         super.onDestroy()
     } // End of fun onDestroy()
 
 
-    private inner class JavaScriptInterface {                           // STEP - 09
+    private inner class JavaScriptInterface {                               // STEP - 09
         @JavascriptInterface
         fun dispKt(webMsg: String) {
             if (webMsg.isNullOrEmpty() || webMsg.isNullOrBlank())
@@ -51,7 +61,7 @@ class KotlinActivity : AppCompatActivity() {
     } // End of JavaScriptInterface class
 
 
-    fun sendToJS() {                                                    // Step - 10
+    fun sendToJS() {                                                        // Step - 10
         myWebView.evaluateJavascript("javascript: "
                 +"dispJS('"+msg.text+"');"
                 ,null
@@ -59,10 +69,21 @@ class KotlinActivity : AppCompatActivity() {
     }
 
 
-    companion object {                                                  // STEP - 01
+    companion object {                                                      // STEP - 01
         private val J_OBJ = "KT"
         private val INNER_URL = "file:///android_asset/WebViewApp/index.html"
         private val BASE_URL = "https://fifthsirean02.github.io/webviewpage1/"
     } // End of Companion Object
+
+    private val isNetworkAvailable: Boolean
+        get() {
+            val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+            return activeNetworkInfo != null && activeNetworkInfo.isConnected
+        }
+
+    fun toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(this.applicationContext, message, duration).show()
+    }
 
 }
